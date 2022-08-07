6a091010a6162e725eda5aaad386625d8021d09d
NguyenTienBMW <nvantien222@gmail.com>
2022-08-07 16:18:46 +0700

    Init project

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,5 @@
+function Header() {
+	return <>Đây là header page</>;
+}
+
+export default Header;
---
+++ src/pages/Features/Home/index.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../../../components";
+
+function Home() {
+	return (
+		<>
+			<Header />
+			<p>Hello 123</p>
+		</>
+	);
+}
+
+export default Home;

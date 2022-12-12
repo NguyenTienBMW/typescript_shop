4e4afec0bffdfb682c1b8d6c22469dad918fc712
trunghuynh <trungbop26092000@gmail.com>
2022-12-12 07:22:27 +0700

    feat: cart and add address

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,30 +1,208 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   BrowserRouter as Router,
   Link,
 } from "react-router-dom";
+import { UserModel } from '../../model/user';
+import { Button, Modal, Form, Input, Select } from 'antd';
+import './style.scss'
+import axios from 'axios';
+import { Command, QueryAPI } from '../../access';
+import { notificationError, notificationSuccess } from '../Noti';
 
-export default function Card() {
+export default function Card({ total, listId }: { total?: number; listId?: number[] }) {
+  const user: any = localStorage.getItem('user');
+  const userInfo: UserModel = JSON.parse(user);
+
+  const [refresh, setRefresh] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [dataAddress, setDataAddress] = useState<any>([]);
+  const [city, setCity] = useState('');
+  const [district, setDistrict] = useState('');
+  const [ward, setWard] = useState('');
+
+  const checkAddress = () => {
+    if (userInfo.address) {
+      alert('ok')
+    } else {
+      showModal()
+    }
+  }
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  useEffect(() => {
+    axios.get(QueryAPI.address.allFull())
+      .then(res => {
+        setDataAddress(res.data)
+      })
+  }, [])
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  const onFinish = (values: any) => {
+    axios({
+      method: 'post',
+      url: Command.address.add(userInfo.id),
+      headers: {},
+      data: values
+    })
+      .then((response) => {
+        if (response.data.code !== '404') {
+          notificationSuccess({ description: 'Bạn đã thêm địa chỉ thành công' });
+          handleSetAddress();
+          handleCancel();
+        } else {
+          notificationError({ description: response.data.message });
+        }
+      }, (error) => {
+        alert(error)
+      });
+  };
+
+  const handleSetAddress = () => {
+    axios.get(QueryAPI.user.single(userInfo.id))
+    .then(res => {
+      localStorage.setItem('user', JSON.stringify(res.data));
+      setRefresh(prev => prev + 1)
+    })
+  }
+
   return (
-    <div className='card-wrap'>
-      <h2 className='card-heading'>
-        Card totals
-      </h2>
-      <table>
-        <tr>
-          <th><h6>Subtotals</h6></th>
-          <th>$240.00</th>
-        </tr>
-        <tr>
-          <td>
-            <h6>Shipping</h6>
-          </td>
-          <td>Free shipping</td>
-        </tr>
-        <th><h6>Total</h6></th>
-        <th><strong>$240.00</strong></th>
-      </table>
-      <Link to="/checkout" className="btn btn-add-prod mt-30">Proceed to checkout</Link>
-    </div>
+    <>
+      <div className='card-wrap'>
+        <h2 className='card-heading'>
+          Card totals
+        </h2>
+        <table>
+          <tr>
+            <th></th>
+            <th>Tổng thanh toán {listId?.length} sản phẩm</th>
+          </tr>
+          <tr>
+            <td>
+              <h6>Shipping</h6>
+            </td>
+            <td>Free shipping</td>
+          </tr>
+          <th><h6>Total</h6></th>
+          <th><strong>{total}</strong></th>
+        </table>
+        <button disabled={!total} onClick={checkAddress} className="btn btn-add-prod mt-30">Mua Hàng</button>
+      </div>
+      <Modal title="Add Address" open={isModalOpen} onOk={handleOk} onCancel={handleCancel} className='add-address-model'>
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          className='form-add-address'
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Full Name"
+            name="name"
+            rules={[{ required: true, message: 'Please input your name!' }]}
+          >
+            <Input placeholder='Full Name' />
+          </Form.Item>
+
+          <Form.Item
+            label="Phone"
+            name="phone"
+            rules={[{ required: true, message: 'Please input your phone!' }]}
+          >
+            <Input placeholder='Phone' />
+          </Form.Item>
+
+          <Form.Item
+            label="City"
+            name="city"
+            rules={[{ required: true, message: 'Please input your city!' }]}
+          >
+            <Select
+              showSearch
+              placeholder="Select a City"
+              options={dataAddress.map((item: any, index: any) => ({
+                label: item.name,
+                value: index
+              }))}
+              value={city}
+              onChange={(value) => setCity(value)}
+            />
+          </Form.Item>
+
+          <Form.Item
+            label="District"
+            name="district"
+            rules={[{ required: true, message: 'Please input your district!' }]}
+          >
+            <Select
+              showSearch
+              disabled={city === ''}
+              placeholder="Select a City"
+              options={dataAddress[Number(city)]?.districts.map((item: any, index: any) => ({
+                label: item.name,
+                value: index
+              }))}
+              onChange={(value) => setDistrict(value)}
+            />
+          </Form.Item>
+
+          <Form.Item
+            label="Ward"
+            name="ward"
+            rules={[{ required: true, message: 'Please input your ward!' }]}
+          >
+            <Select
+              showSearch
+              disabled={district === ''}
+              placeholder="Select a City"
+              options={dataAddress[Number(city)]?.districts[Number(district)]?.wards?.map((item: any, index: any) => ({
+                label: item.name,
+                value: index
+              }))}
+              onChange={(value) => setWard(value)}
+            />
+          </Form.Item>
+
+          <Form.Item
+            label="Address"
+            name="address"
+            rules={[{ required: true, message: 'Please input your address!' }]}
+          >
+            <Input placeholder='address' />
+          </Form.Item>
+
+
+          <div className='modal-footer-container'>
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }} className='form-footer'>
+              <Button type="ghost" onClick={handleCancel}>
+                Cancel
+              </Button>
+            </Form.Item>
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }} className='form-footer'>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </div>
+        </Form>
+      </Modal>
+    </>
   )
 }
--- src/pages/Features/Cart/index.tsx
+++ src/pages/Features/Cart/index.tsx
@@ -2,35 +2,76 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { QueryAPI } from '../../../access'
 import { BannerFooter, BreadCrumb, Card, Contact, Footer, Header, Product_List } from '../../../components'
-import {Table} from '../../../components/Table'
+import { Table } from '../../../components/Table'
 import { CartModel, ProductModel } from '../../../model'
 import { UserModel } from '../../../model/user'
 
-type NewCart = ProductModel & CartModel
 export default function Cart() {
   const user: any = localStorage.getItem('user');
   const userInfo: UserModel = JSON.parse(user);
-  const [productList, setProductList] = useState<NewCart[]>([])
+  const [cartList, setCartList] = useState<Record<string, CartModel[]>>({})
+  const [totalPrice, setTotalPrice] = useState(0)
+  const [refresh, setRefresh] = useState(0)
+  const [listIdSelect, setListIdSelect] = useState<number[]>([])
 
   useEffect(() => {
     axios.get(QueryAPI.cart.all(userInfo.id))
       .then(res => {
-        setProductList(res.data)
+        const data: CartModel[] = res.data;
+        const shopIdContainer: number[] = [];
+        const result: Record<string, CartModel[]> = {}
+
+        data.forEach((item: CartModel) => {
+          if (shopIdContainer.includes(item.shop_id)) return
+          shopIdContainer.push(item.shop_id)
+        })
+
+        shopIdContainer.forEach(item => {
+          if (!result[String(item)]) result[String(item)] = [];
+          data.forEach((itemData: CartModel) => {
+            if (itemData.shop_id === Number(item)) {
+              result[String(item)] = [
+                ...result[String(item)],
+                itemData
+              ]
+            }
+          })
+        })
+        setCartList(result)
       })
       .catch(err => {
         console.log(err)
       })
-  }, [])
+  }, [refresh])
+
+  const onSelect = (listid: number[], total: number) => {
+    setTotalPrice(total)
+    setListIdSelect(listid)
+  }
+
+  const onUpdateQuanlity = (shopId: number, cartId: number, value: number) => {
+    setCartList(prev => {
+      prev[shopId][cartId].quanlity = value
+      return {...prev}
+    })
+  }
+
+  // const onDelete =()
 
   return (
     <div>
       <div className="container">
         <div className="row table-wrap">
-          <div className="col-8">
-            <Table productList={productList} />
+          <div className="col-9">
+            {Object.keys(cartList).length > 0 && <Table
+              cartList={cartList}
+              onDelete={() => setRefresh(prev => prev + 1)}
+              onUpdateQuanlity={onUpdateQuanlity}
+              onSelect={onSelect}
+            />}
           </div>
-          <div className="col-4">
-            <Card />
+          <div className="col-3">
+            <Card total={totalPrice} listId={listIdSelect} />
           </div>
         </div>
       </div>
